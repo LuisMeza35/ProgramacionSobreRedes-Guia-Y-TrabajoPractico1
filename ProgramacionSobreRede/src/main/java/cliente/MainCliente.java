82954cf5eecf7bbd4bd5a943ac716156d42ad7da
LuisMeza35 <92602170+LuisMeza35@users.noreply.github.com>
2023-11-15 20:45:07 -0300

    commit

---
+++ ProgramacionSobreRede/src/main/java/cliente/MainCliente.java
@@ -0,0 +1,15 @@
+package cliente;
+
+
+public class MainCliente{
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Cliente c = new Cliente();
+
+        c.clienteOn();
+
+    }
+
+}
---
+++ ProgramacionSobreRede/src/main/java/servidor/MainServidor.java
@@ -0,0 +1,14 @@
+package servidor;
+
+public class MainServidor {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Servidor s = new Servidor();
+
+        s.serverOn();
+
+    }
+
+}
